9eeee484bff0e4995cd88b8768056154f1c11ef6
dangmakara <dang.dangmakara@gmail.com>
2024-11-26 15:56:14 +0700

    update

---
+++ src/app/pages/create-order/create-order.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-order',
+  imports: [],
+  templateUrl: './create-order.component.html',
+  styleUrl: './create-order.component.css'
+})
+export class CreateOrderComponent {
+
+}
